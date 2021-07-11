55ba7805e7bce00149ec07dff910059f1f2fc63a
QinHaoting <1165101405@qq.com>
2021-07-11 18:51:46 +0800

    秦浩廷，更新了sac_app中的urls

--- Student_Activity_Center/sac_app/urls.py
+++ Student_Activity_Center/sac_app/urls.py
@@ -16,11 +16,9 @@ urlpatterns = [
     path('login/register/', views.register),
     # 1 忘记密码
     path('login/forgetpwd/', views.forgetpwd),
-
-
     # 学生:主页
     path('stu_home/', views.stu_home),
-    # 2学生:活动大厅
+    # 2学生: 活动大厅
     path('stu_home/stu_activity/', views.stu_activity),
     # 2学生：已参加活动页
     path('stu_home/stu_join_activity/', views.stu_join_activity),
@@ -28,7 +26,7 @@ urlpatterns = [
     path('stu_home/stu_center/', views.stu_center, name="stu_center"),
     # 2 1学生：可参加活动页
     path('stu_home/stu_activity_yes/', views.stu_activity_yes, name="stu_activity_yes"),
-    # 2 学生：不可参加活动
+    # 2 学生：不可参加活动  - （秦浩廷）
     path('stu_home/stu_activity_no/', views.stu_activity_no, name="stu_activity_no"),
     # 1 学生：创建队伍
     path('stu_home/stu_createteam/', views.stu_createteam, name="stu_create_team"),
@@ -47,7 +45,7 @@ urlpatterns = [
     path('org_home/org_launch_notice/', views.org_launch_notice, name="org_launch_notice"),
     # 1 2组织者：修改活动
     path('org_home/org_modify_activity/', views.org_modify_activity, name="org_modify_activity"),
-    # 2 组织者：查看已发活动
+    # 2 组织者：查看已发活动 - （秦浩廷）
     path('org_home/org_view_pasted_activity/', views.org_view_pasted_activity, name="org_view_pasted_activity"),
 
 
@@ -55,6 +53,7 @@ urlpatterns = [
     path('mag_home/', views.mag_home, name="mag_home"),
     # 1 2管理者：审核
     path('mag_home/mag_examine/', views.mag_examine, name="mag_examine"),
+    # 2 管理者：审核功能
     # 1 2管理者：管理组织者
     path('mag_home/mag_manage/', views.mag_manage, name="mag_manage"),
 
