d230c20d6848aee84e6bed865c544cc31342e40c
马祥 <8376726+Mxmevol@user.noreply.gitee.com>
2021-07-14 22:52:29 +0800

    马祥，更新了models

--- Student_Activity_Center/sac_app/models.py
+++ Student_Activity_Center/sac_app/models.py
@@ -5,7 +5,8 @@ import datetime
 
 # 学生类
 class students(models.Model):
-    stu_id = models.CharField(max_length=128)
+    # 学生账号
+    stu_id = models.CharField(max_length=64)
     # 学生姓名
     stu_name = models.CharField(max_length=64)
     # 学生登入密码
@@ -28,36 +29,9 @@ class students(models.Model):
     stu_valid = models.IntegerField()
 
 
-# 管理员类
-class managers(models.Model):
-    man_id = models.CharField(max_length=128)
-    # 管理员账号
-    man_name = models.CharField(max_length=64)
-    # 管理员登入密码
-    man_password = models.CharField(max_length=64)
-
-
-# 组织类
-class organizers(models.Model):
-    org_id = models.CharField(max_length=128)
-    # 组织账号
-    org = models.CharField(max_length=64)
-    # 组织姓名
-    org_name = models.CharField(max_length=64, blank=False)
-    # 组织负责人姓名
-    org_header_name = models.CharField(max_length=64, blank=False)
-    # 组织账号密码
-    org_password = models.CharField(max_length=128)
-    # 组织负责人联系方式
-    org_header_phone = models.CharField(max_length=32, blank=False)
-    # 组织负责人所在学院
-    org_header_college = models.CharField(max_length=128, null=True)
-    # 组织简介
-    org_introduction = models.TextField(blank=False)
-
-
 # 活动类
 class activities(models.Model):
+    # 活动ID
     act_id = models.CharField(max_length=128)
     # 活动名称
     act_name = models.CharField(max_length=64, blank=False)
@@ -66,9 +40,65 @@ class activities(models.Model):
     # 活动结束时间
     act_end_time = models.DateField(default=1970 - 1 - 1, blank=False)
     # 活动负责人名字
-    act_organizer_name = models.ForeignKey(organizers, blank=False, on_delete=models.PROTECT)
+    act_organizer_name = models.CharField(max_length=64, null=True)
+    # 活动社团表间关系
+    act_organizer = models.ForeignKey('organizers', on_delete=models.PROTECT)
     # 活动负责人联系方式
     act_organizer_phone = models.CharField(max_length=32, blank=False)
+    # 活动类型 单人0 ，组队1
+    act_type = models.IntegerField()
+    # 活动已创建队伍数
+    act_created_team_number = models.IntegerField(null=True)
+    # 活动组队最多人数
+    act_max_team_number = models.IntegerField(null=True)
+    # 活动组队最少人数
+    act_min_team_number = models.IntegerField(null=True)
+    # 总的活动组数
+    act_team_number = models.IntegerField(null=True)
+    # 活动进行状态
+    act_state = models.IntegerField(null=True)
+    # 活动总人数
+    act_total_number = models.IntegerField(null=True)
+    # 活动已参加人数
+    act_participated_number = models.IntegerField(null=True)
+    # 活动报名剩余组数
+    act_available_team_number = models.IntegerField(null=True)
+    # 活动报名剩余人数
+    act_available_number = models.IntegerField(null=True)
+    # 活动能否参加状态
+    act_flag = models.CharField(max_length=16, blank=False)
+    # 活动附件
+    act_planning_book = models.FileField(null=True)
+    # 活动简介
+    act_introduction = models.TextField(blank=False)
+    # 多对多表间关系
+    # 参加活动人员名单
+    act_to_stu_table = models.ManyToManyField(students, through='act_to_stu')
+    # 一对多表间关系
+    # 参加活动队伍名单
+    # act_to_stu_team_table = models.ManyToManyField(teams, through='act_to_team')
+
+
+# 修改活动信息中间表
+class activities_modified(models.Model):
+    # 活动ID
+    act_id = models.CharField(max_length=128)
+    # 活动名称
+    act_name = models.CharField(max_length=64, blank=False)
+    # 活动开始时间
+    act_start_time = models.DateField(default=1970 - 1 - 1, blank=False)
+    # 活动结束时间
+    act_end_time = models.DateField(default=1970 - 1 - 1, blank=False)
+    # 活动负责人名字
+    act_organizer_name = models.CharField(max_length=64, null=True)
+    # 活动社团表间关系
+    # act_organizer = models.ForeignKey('organizers', on_delete=models.PROTECT)
+    # 活动负责人联系方式
+    act_organizer_phone = models.CharField(max_length=32, blank=False)
+    # 活动类型 单人0 ，组队1
+    act_type = models.IntegerField()
+    # 活动已创建队伍
+    act_created_team_number = models.IntegerField()
     # 活动组队最多人数
     act_max_team_number = models.IntegerField()
     # 活动组队最少人数
@@ -91,10 +121,50 @@ class activities(models.Model):
     act_introduction = models.TextField(blank=False)
     # 多对多表间关系
     # 参加活动人员名单
-    act_to_stu_table = models.ManyToManyField(students, through='act_to_stu')
     # 一对多表间关系
     # 参加活动队伍名单
     # act_to_stu_team_table = models.ManyToManyField(teams, through='act_to_team')
+
+
+# 管理员类
+class managers(models.Model):
+    # 管理员账号
+    man_id = models.CharField(max_length=64)
+    # 管理员登入密码
+    man_password = models.CharField(max_length=64)
+
+
+# 组织类
+class organizers(models.Model):
+    # 组织账号
+    org_id = models.CharField(max_length=64)
+    # 组织姓名
+    org_name = models.CharField(max_length=64, blank=False)
+    # 组织负责人姓名
+    org_header_name = models.CharField(max_length=64, blank=False)
+    # 组织账号密码
+    org_password = models.CharField(max_length=128)
+    # 组织负责人联系方式
+    org_header_phone = models.CharField(max_length=32, blank=False)
+    # 组织负责人所在学院
+    org_header_college = models.CharField(max_length=128, null=True)
+    # 组织简介
+    org_introduction = models.TextField(blank=False)
+    # 组织信息有效标志： 0-删除； 1-有效
+    org_valid = models.IntegerField()
+    # 承办的活动
+    # org_act = models.ForeignKey(activities, on_delete=models.PROTECT)
+
+
+# 组织修改组织信息中间表单
+class organizers_modified(models.Model):
+    org_id = models.CharField(max_length=64)
+    org_password = models.CharField(max_length=128)
+    org_name = models.CharField(max_length=64)
+    org_header_name = models.CharField(max_length=64)
+    org_header_phone = models.CharField(max_length=32)
+    org_introduction = models.TextField()
+    org_valid = models.IntegerField()
 
 
 # 活动已参加学生子表类
@@ -125,11 +195,11 @@ class teams(models.Model):
     team_members = models.ManyToManyField(students, through="stu_to_team")
     # 队伍参加活动的名称
     team_act = models.ForeignKey(activities, on_delete=models.PROTECT)
+    team_introduction = models.CharField(max_length=256,null=True)
 
 
 # 公告类
 class notices(models.Model):
-    notice_id = models.CharField(max_length=128)
     # 公告标题
     notice_title = models.CharField(max_length=64, blank=False)
     # 创建公告时间
@@ -138,8 +208,10 @@ class notices(models.Model):
     notice_content = models.TextField(blank=False)
     # 公告附件
     notice_appendix = models.FileField(null=True)
+    # 公告类型
+    notice_tag = models.IntegerField(max_length=1)
 
-    notice_tag = models.IntegerField()
+
 # 学生队伍子表类
 class stu_to_team(models.Model):
     stu = models.ForeignKey(students, on_delete=models.PROTECT)
